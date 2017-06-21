3cf2319c13257017615a184c155f77d9932055fb
Cao Xiaoqi <xqcao@thoughtworks.com>
2017-06-21 18:01:30 +0800

    he

--- app/jobs/hello_job.rb
+++ app/jobs/hello_job.rb
@@ -2,6 +2,9 @@ class HelloJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    
+    File.open('a.text', "w+") do |f|
+      f.write('hello job')
+    end
+    Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{args.inspect}"
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module HerokuRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :delayed_job
   end
 end
