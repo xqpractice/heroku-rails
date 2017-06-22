f87cb6d66f77ce8475910567f1fdd4d25d68a00f
Cao Xiaoqi <xqcao@thoughtworks.com>
2017-06-22 15:51:11 +0800

    Update job

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,6 +2,16 @@ class TestJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{args.inspect}"
+    Rails.logger.debug "#{self.class.name}: I'm performing test job with arguments: #{args.inspect}"
+  end
+
+  def after_perform()
+    TestJob.set(wait: 5.minutes).perform_later(1, 2, 3)
+  end
+
+  def reschedule_at(current_time, attempts)
+    current_time + 30.seconds
   end
 end
+
+TestJob.set(wait: 5.minutes).perform_later(1, 2, 3)
